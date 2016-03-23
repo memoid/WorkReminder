00b30ed56b09d5ad481628260eb4593d654b84e1
gmoro <gmo.rodriguez.m@gmail.com>
2016-03-22 21:45:12 -0400

    Probando subida

---
+++ app/src/main/java/com/memoid/workreminder/DailyMailActivity.java
@@ -0,0 +1,20 @@
+package com.memoid.workreminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DailyMailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_mail);
+    }
+
+    public void sendMail(View view) {
+
+
+
+    }
+}
